c16456f494e75124fd32a0b57c3b23d1f1962b5f
Tien Dat Duong <datdt@vietnamlab.vn>
2025-08-22 18:27:41 +0700

    Vietnamese text

---
+++ lang/vi/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Thông tin đăng nhập không đúng.',
+    'password' => 'Mật khẩu không chính xác.',
+    'throttle' => 'Quá nhiều lần đăng nhập. Vui lòng thử lại sau :seconds giây.',
+];
---
+++ lang/vi/backend.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'welcome' => 'Chào mừng bạn đến với hệ thống quản trị!',
+    'logout' => 'Đăng xuất',
+    'login' => 'Đăng nhập',
+    'dashboard' => 'Bảng điều khiển',
+];
